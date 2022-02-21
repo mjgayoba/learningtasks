72f3b0cfa690a89157eeccc7f98133e09b95c3db
mjgayoba <100089314+mjgayoba@users.noreply.github.com>
2022-02-21 11:57:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Hello guys. My name is Mar Jan. Welcome to my Website! </h1>
+  <ul>
+    <li><a href="Homepage.php" target="_blank"> HOME </a></li>
+    <li><a href="LT2/index.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+  </ul>
+  </body>
+</html>
