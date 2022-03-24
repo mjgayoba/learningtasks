6ecf378bcd4321fa99425e765660479ede0fc38b
mjgayoba <100089314+mjgayoba@users.noreply.github.com>
2022-03-25 01:45:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <li><a href="/index.php" target="_blank"> HOME </a></li>
     <li><a href="/LT2/index.html" target= "_blank"> LEARNING TASK 2 </a> </li>
     <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="/CLE1MT/index.html" target= "_blank"> CLE1: CULMINATING LEARNING EVIDENCE </a> </li>
   </ul>
   </body>
 </html>
